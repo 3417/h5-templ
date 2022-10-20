e60e4f0ba79a5bccca8e7fbc5c6c1b83edc1192b
liangdong <1789719643@qq.com>
2022-10-20 16:37:49 +0800

    fix:修改

--- vue.config.js
+++ vue.config.js
@@ -2,14 +2,20 @@ const {
   defineConfig
 } = require('@vue/cli-service')
 const path = require('path')
+const FileManagerPlugin = require('filemanager-webpack-plugin')
+const TerserPlugin = require("terser-webpack-plugin");
 function resolve(dir) {
   return path.join(__dirname, dir)
 }
+const timestamp = new Date().getTime();
+const fileName = 'dist';
 module.exports = defineConfig({
   transpileDependencies: true,
+  outputDir: `${fileName}`,
   lintOnSave: false,
   publicPath: "./",
   assetsDir: "static",
+  productionSourceMap: false,
   css: {
     sourceMap: true,
     loaderOptions: {
@@ -18,9 +24,35 @@ module.exports = defineConfig({
       }
     }
   },
-  configureWebpack:(config)=>{
+  configureWebpack: (config) => {
     config.optimization = {
-      nodeEnv:false
+      nodeEnv: false,
+      minimizer: [
+        new TerserPlugin({
+          extractComments: false
+        })
+      ]
+    }
+    if (process.env.NODE_ENV === 'production') {
+      config.output.filename = `js/[name].${timestamp}.js`
+      config.output.chunkFilename = `js/[name].${timestamp}.js`
+      config.plugins.push(
+        new FileManagerPlugin({ //初始化 filemanager-webpack-plugin 插件实例
+          events: {
+            onEnd: {
+              delete: [ //首先需要删除项目根目录下的dist.zip
+                `./${fileName}.zip`,
+              ],
+              archive: [ //然后我们选择dist文件夹将之打包成dist.zip并放在根目录
+                {
+                  source: `./${fileName}`,
+                  destination: `./${fileName}.zip`,
+                  format: 'zip'
+                },
+              ]
+            }
+          }
+        }))
     }
   },
   chainWebpack: config => {
@@ -37,7 +69,7 @@ module.exports = defineConfig({
       return args
     })
   },
-  devServer:{
+  devServer: {
     proxy: {
       '/api': {
         target: 'http://www.baidu.com', // 后台接口域名
