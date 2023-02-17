4d3296cfacca23907b914c0715479ee54dacb6be
liangdong <1789719643@qq.com>
2023-02-17 14:36:35 +0800

    fix:修改

--- src/components/layer/index.js
+++ src/components/layer/index.js
@@ -1,50 +1,49 @@
 import popupCpm from './popupcpm.vue';
 let layPopup = {};
 layPopup.install = function (Vue, options) {
-    let Action = Vue.extend(popupCpm);
-    const div = document.createElement("div");
-    document.body.appendChild(div);
     let $ele;
     const Mask = (config) => {
+        let Action = Vue.extend(popupCpm);
+        const div = document.createElement("div");
+        document.body.appendChild(div);
         $ele = new Action({
-            propsData:{
+            propsData: {
                 ...config,
             },
-    
         }).$mount(div)
     }
 
-    const _destory = ()=>{
+    const _destory = () => {
         $ele.popupShow = false;
         $ele.onClose();
         $ele = null
     }
     Vue.prototype.vshow = (opts) => {
         if ($ele) return;
-        switch(typeof opts){
+        switch (typeof opts) {
             case 'string':
                 console.log('Oops... is String');
                 break;
             case 'object':
                 Mask(opts);
-                for(let key in opts){
-                    if(typeof opts[key] === 'function'){
-                        $ele[key] = (v)=>{
-                            if(opts.isOwnDestory){opts[key](v,_destory)}
-                            else {opts[key](v);_destory()};
+                for (let key in opts) {
+                    if (typeof opts[key] === 'function') {
+                        $ele[key] = (v) => {
+                            if (opts.isOwnDestory) { opts[key](v, _destory) }
+                            else { opts[key](v); _destory() };
                         }
                     }
                 }
                 break;
         }
-        return $ele.vshow().then(_rsp=>{
+        return $ele.vshow().then(_rsp => {
             $ele = null;
-        }).catch(_err=>{
+        }).catch(_err => {
             $ele = null;
         })
     }
     // 全局销毁弹窗事件
-    Vue.prototype.vshowDestory = ()=>{
+    Vue.prototype.vshowDestory = () => {
         _destory();
     }
 }
